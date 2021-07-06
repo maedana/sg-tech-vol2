56c120bca96891c2145bd67c99426d580a811980
maedana <maeda.na@gmail.com>
2021-07-06 13:54:25 +0900

    FizzBuzzのテスト追加

--- fizzbuzz/src/main.rs
+++ fizzbuzz/src/main.rs
@@ -13,13 +13,13 @@ fn main() {
 }
 
 fn fizzbuzz(number: i32) -> String {
-    if number % 3 == 0 {
-        "Fizz".to_string()
-    } else if number % 5 == 0 {
-        "Buzz".to_string()
-    } else {
-        "".to_string()
+    if number % 3 == 0 && number % 5 == 0 {
+        return "FizzBuzz".to_string();
     }
+    if number % 3 == 0 {
+        return "Fizz".to_string();
+    }
+    return "Buzz".to_string();
 }
 
 #[cfg(test)]
@@ -34,5 +34,10 @@ mod tests {
     #[test]
     fn print_buzz() {
         assert_eq!(fizzbuzz(5), "Buzz");
+    }
+
+    #[test]
+    fn print_fizzbuzz() {
+        assert_eq!(fizzbuzz(15), "FizzBuzz");
     }
 }
